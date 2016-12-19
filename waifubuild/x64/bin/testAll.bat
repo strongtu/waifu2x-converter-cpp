b961e58304f4dbc4b3b9446737995210f0ec56d5
strongtu <tuqbasic@qq.com>
2016-12-19 23:50:55 +0800

    Add Images and Upgrade batch

--- waifubuild/x64/bin/testAll.bat
+++ waifubuild/x64/bin/testAll.bat
@@ -1,4 +1,2 @@
-@call testBlock
-@call testBlock waifu2x-converter.exe waifu
 @call testMode
 @call testMode waifu2x-converter.exe waifu
--- waifubuild/x64/bin/testMode.bat
+++ waifubuild/x64/bin/testMode.bat
@@ -1,6 +1,7 @@
 @echo off
 set exe=%1%
 set fold=%2%
+set IMG_DIR=".\imgs"
 
 if "%exe%"=="" (
 	set exe=waifubuild.exe
@@ -8,18 +9,8 @@ if "%exe%"=="" (
 )
 @echo on
 
-%exe% --disable-gpu -j 2 -m noise_scale --scale_ratio 2 --noise_level 2 -i imgs/o1.jpg -o o/imgs_%fold%/ns_2.jpg > o/imgs_%fold%/ns_2.txt
-%exe% --disable-gpu -j 4 -m noise_scale --scale_ratio 2 --noise_level 2 -i imgs/o1.jpg -o o/imgs_%fold%/ns_4.jpg > o/imgs_%fold%/ns_4.txt
-%exe% --disable-gpu -j 8 -m noise_scale --scale_ratio 2 --noise_level 2 -i imgs/o1.jpg -o o/imgs_%fold%/ns_8.jpg > o/imgs_%fold%/ns_8.txt
-
-%exe% --disable-gpu -j 2 -m noise --noise_level 2 -i imgs/o1.jpg -o o/imgs_%fold%/n_2.jpg > o/imgs_%fold%/n_2.txt
-%exe% --disable-gpu -j 4 -m noise --noise_level 2 -i imgs/o1.jpg -o o/imgs_%fold%/n_4.jpg > o/imgs_%fold%/n_4.txt
-%exe% --disable-gpu -j 8 -m noise --noise_level 2 -i imgs/o1.jpg -o o/imgs_%fold%/n_8.jpg > o/imgs_%fold%/n_8.txt
-
-%exe% --disable-gpu -j 2 -m scale --scale_ratio 2 -i imgs/o1.jpg -o o/imgs_%fold%/s_2.jpg > o/imgs_%fold%/s_2.txt
-%exe% --disable-gpu -j 4 -m scale --scale_ratio 2 -i imgs/o1.jpg -o o/imgs_%fold%/s_4.jpg > o/imgs_%fold%/s_4.txt
-%exe% --disable-gpu -j 8 -m scale --scale_ratio 2 -i imgs/o1.jpg -o o/imgs_%fold%/s_8.jpg > o/imgs_%fold%/s_8.txt
-
-%exe% -m noise_scale --scale_ratio 2 --noise_level 2 -i imgs/o1.jpg -o o/imgs_%fold%/gns.jpg > o/imgs_%fold%/gns.txt
-%exe% -m noise                                --noise_level 2 -i imgs/o1.jpg -o o/imgs_%fold%/gn.jpg   > o/imgs_%fold%/gn.txt
-%exe% -m scale           --scale_ratio 2                       -i imgs/o1.jpg -o o/imgs_%fold%/gs.jpg     > o/imgs_%fold%/gs.txt
+for /f "delims=\" %%i in ('dir /b /a-d /o-d "%IMG_DIR%\*.jpg" ') do (
+%exe% -m scale --scale_ratio 2 -i %IMG_DIR%\%%i -o o\imgs_%fold%\%%i_s.jpg
+%exe% -m noise --noise_level 2 -i o\IMG_DIR\%%i_s.jpg -o o\imgs_%fold%\%%i_n.jpg
+%exe% -m noise_scale --scale_ratio 2 --noise_level 2 -i %IMG_DIR%\%%i -o o\imgs_%fold%\%%i_ns.jpg
+)
